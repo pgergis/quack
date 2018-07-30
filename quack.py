428d949f654ec77fbb57782c5802a22c18e5f288
Pierre Gergis <pierre.s.gergis@gmail.com>
2018-07-30 16:45:35 -0400

    Auth setup

--- quack.py
+++ quack.py
@@ -1,3 +1,12 @@
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+import os
+CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
 import tweepy
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
