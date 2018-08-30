c0fdcdf0ffa6082b8b87915f2f5332342f64718a
Pierre Gergis <pierre.s.gergis@gmail.com>
2018-08-30 17:42:39 -0400

    Some more toying around -- nothing to see here

--- quack.py
+++ quack.py
@@ -8,15 +8,45 @@ ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 import tweepy
+import json
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-tweets = api.home_timeline()
-for tweet in tweets:
-    print(tweet.text)
+tweet_archive = []
+channels = {}
 
-tweets = api.user_timeline()
-for tweet in tweets:
-    print(tweet.text)
+following = []
+user_prefs = {}
+
+for friend in tweepy.Cursor(api.friends).items():
+    following.append(json.loads(friend._json))
+
+# def build_channel(channel_name, channel_criteria):
+#     channels[channel_name] = channel_criteria
+
+# def populate_channels(tweet):
+#     for channel, criteria in channels.items():
+#         for criterion, filters in criteria.items():
+#             for each filt in filters:
+#                 if criterion == 'text':
+#                     if filt in tweet['text']:
+#                         channel
+
+
+# def parse_tweet(status):
+#     tweet = json.loads(status)
+#     return tweet
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        tweet_archive.append(parse_tweet(status))
+
+# build_channel("dog media",{'text':["dog","puppy"],'hashtags':['#dog','puppies'],'entities':['media']})
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
+
+myStream.filter(filter_level=['medium'],follow=following)
